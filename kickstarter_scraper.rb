e66a1afe100222df7a1607c5cc36909387584b4a
Charles Pelton <charliejp0311@gmail.com>
2020-02-19 15:30:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,6 +3,8 @@ require "nokogiri"
 
 def create_project_hash
   # write your code here
+  html = file.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
   project_hash = {}
 
 end
